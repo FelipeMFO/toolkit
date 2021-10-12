49b4e609eec9a3960f836407a77456fcd46eb21a
FelipeMFO <fmfoapo@gmail.com>
2021-10-13 00:02:57 +0200

    Create Processing class

---
+++ src/processing/Processing.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import copy
+
+
+class Processing():
+    """Processes processing .
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def drop_rows_by_occurrence(
+        self, df: pd.DataFrame,
+        column: str, threshold: int,
+        type: str = "less"
+    ) -> pd.DataFrame:
+        """Drop rows by occurrence of a given occurrence .
+
+        Args:
+            df (pd.DataFrame): [description]
+            column (str): [description]
+            value (int): [description]
+            type (str, optional): [description]. Defaults to "less".
+
+        Returns:
+            pd.DataFrame: [description]
+        """
+        if type == "less":
+            df_ans = df[df[column].map(df[column].value_counts()) > threshold]
+        else:
+            df_ans = df[df[column].map(df[column].value_counts()) < threshold]
+
+        return df_ans
